e5ad4cd44295b0824c5792f284b3dfdceb2507be
Kyle Hatch <ky13h4tch@gmail.com>
2020-01-16 11:11:54 -0700

    Updated store value

--- src/store/store.js
+++ src/store/store.js
@@ -20,7 +20,7 @@ const store = new Vuex.Store({
     direction: "",
     callState: "",
     timeOfCall: "",
-    showCallPopState: true,
+    showCallPopState: false,
     savedContacts: []
   },
   getters: {
