1ce475f0c8a5fd66bc1c741510fcf268bcdbeef6
Kyle Hatch <ky13h4tch@gmail.com>
2020-01-17 09:59:30 -0700

    Updated image on callpop

--- src/components/CallPop.vue
+++ src/components/CallPop.vue
@@ -5,11 +5,9 @@
         <v-col>
           <v-card elevation="8" max-width="344" class="mx-auto">
             <v-list-item three-line>
-              <v-list-item-avatar size="60"
-                ><v-icon size="80"
-                  >mdi-account-circle</v-icon
-                ></v-list-item-avatar
-              >
+              <v-list-item-avatar size="60">
+                <img src="../assets/LMI_Jive_Primary_HEX.png" />
+              </v-list-item-avatar>
               <v-list-item-content>
                 <v-list-item-title class="headline"
                   >{{ firstName || "No contact" }} {{ lastName }}
